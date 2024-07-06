efa5e06600c9edb97c753a777008d00b9bcae3ab
Kanika1609 <ch1221471@iitd.ac.in>
2024-07-06 18:43:49 +0530

    Add files via upload

--- Sudoku_Solver.cpp
+++ Sudoku_Solver.cpp
@@ -4,75 +4,69 @@
 
 using namespace std;
 
-bool isSafe(int row,int col,vector<vector<int>> &board,int value ){
-    for(int i = 0;i<9;i++){
-        //row check
-        if(board[row][i] == value){
+bool isSafe(int row, int col, vector<vector<int>>& board, int value) {
+    for (int i = 0; i < 9; i++) {
+        // Row check
+        if (board[row][i] == value) {
             return false;
         }
-        //col check
-        if(board[i][col] == value){
+        // Column check
+        if (board[i][col] == value) {
             return false;
         }
-
-        // 3*3 matrix
-        if(board[3*(row/3) + i/3][3*(col/3) + i%3] == value){
+        // 3x3 matrix check
+        if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == value) {
             return false;
         }
     }
-
     return true;
 }
 
-bool solve(vector<vector<int>> &board){
+bool solve(vector<vector<int>>& board) {
     int n = 9;
 
-    for(int row = 0; row<9;row++){
-        for(int col = 0;col<9;col++){
-            //cell empty
-            if(board[row][col] == 0){
-                for(int val = 1;val<=9;val++){
-                    if(isSafe(row,col,board,val)){
+    for (int row = 0; row < 9; row++) {
+        for (int col = 0; col < 9; col++) {
+            // Cell empty
+            if (board[row][col] == 0) {
+                for (int val = 1; val <= 9; val++) {
+                    if (isSafe(row, col, board, val)) {
                         board[row][col] = val;
-                        //recursive call
+                        // Recursive call
                         bool aagesolpossible = solve(board);
-                        if(aagesolpossible){
+                        if (aagesolpossible) {
                             return true;
                         }
-                        else{
-                            //bactracking
+                        else {
+                            // Backtracking
                             board[row][col] = 0;
                         }
                     }
                 }
                 return false;
             }
-
         }
-        
     }
     return true;
 }
 
-void solveSudoku(vector<vector<int>> &sudoko){
-    solve(sudoko);
-
-
+void solveSudoku(vector<vector<int>>& sudoku) {
+    solve(sudoku);
 }
 
+void inputSudoku(vector<vector<int>>& sudoku) {
+    cout << "Enter the Sudoku puzzle (0 for empty cells):" << endl;
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            cin >> sudoku[i][j];
+        }
+    }
+}
 
 int main() {
-    vector<vector<int>> sudoku = {
-        {5, 3, 0, 0, 7, 0, 0, 0, 0},
-        {6, 0, 0, 1, 9, 5, 0, 0, 0},
-        {0, 9, 8, 0, 0, 0, 0, 6, 0},
-        {8, 0, 0, 0, 6, 0, 0, 0, 3},
-        {4, 0, 0, 8, 0, 3, 0, 0, 1},
-        {7, 0, 0, 0, 2, 0, 0, 0, 6},
-        {0, 6, 0, 0, 0, 0, 2, 8, 0},
-        {0, 0, 0, 4, 1, 9, 0, 0, 5},
-        {0, 0, 0, 0, 8, 0, 0, 7, 9}
-    };
+    vector<vector<int>> sudoku(9, vector<int>(9, 0));
+
+    inputSudoku(sudoku);
 
     solveSudoku(sudoku);
 
